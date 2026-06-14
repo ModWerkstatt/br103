b11b9a0d6ffe38814410aaeda25aa678666b4c08
Florian Thiel <flth@flth.de>
2026-06-14 22:56:59 +0200

    korrigiere Soundset

--- vehicle/train/br103/sound/br103.snd.lua
+++ vehicle/train/br103/sound/br103.snd.lua
@@ -15,7 +15,7 @@ soundsetutil.addTrackParam01(data, "br103/stand.wav", 25.0,
 			{ 0.01, .0 },
 			{ 1.0, .0 }
         },
-		{vehicle, "speed01"}
+		{"vehicle", "speed01"}
 )
 
 soundsetutil.addTrackParam01(data, "br103/lauf.wav", 25.0,
@@ -28,7 +28,7 @@ soundsetutil.addTrackParam01(data, "br103/lauf.wav", 25.0,
 			{ 0.0, 1.0 },
 			{ 1.0, 1.5 }
         },
-		{vehicle, "speed01"}
+		{"vehicle", "speed01"}
 )
 
 
@@ -43,7 +43,7 @@ soundsetutil.addTrackParam01(data, "br103/luefter.wav", 25.0,
 			{ .0, 1.0 },
 			{ 1.0, 1.1 }
         },
-		{vehicle, "speed01"}
+		{"vehicle", "speed01"}
 )
 
 soundsetutil.addTrackSqueal(data, "br103/kurve.wav", 25.0)
